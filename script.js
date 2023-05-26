718a53587001e1b58c64d9fa5be7c8cc17eb05d2
aflacc <60161366+aflacc@users.noreply.github.com>
2023-05-26 21:40:43 +0000

    THEY. CAN. MOVE!

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// note to self: re-export the gifs so they dont have uneeded space in the image so moving them around doesn't become annoying
+
 var objects = { // in different arrays so i can use the funny individual counters :3
 	cat: [],
 	campfire: []
@@ -5,6 +7,7 @@ var objects = { // in different arrays so i can use the funny individual counter
 var preview = document.getElementById("placement_preview");
 var root = document.querySelector(':root');
 var darktheme = false;
+var seen_warning = false; // for having too many objects
 
 var images = {
 	"campfire": "https://github.com/aflacc/campfire-cats/blob/main/assets/campfire.gif?raw=true",
@@ -14,6 +17,48 @@ var images = {
 // math.random is weird
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
+}
+// brain hurt, thank you w3schools
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+		elmnt.style["z-index"] = (elmnt.offsetTop - pos2);
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
 
 // unused at the moment, keeping it in case i want to use it later
@@ -53,14 +98,14 @@ function toggle_theme(actuallyChange) {
 }
 // objec t :)
 class Object {
-	constructor(type,x,y,flipped = false) {
+	constructor(type,x,y,flipped) {
 		this.type = type;
-		if (x != null) {this.x = x;} else {this.x = getRandomInt(640);}
-		if (y != null) {this.y = y;} else {this.y = getRandomInt(360);}
+		if (x != null) {this.x = x;} else {this.x = getRandomInt(600);}
+		if (y != null) {this.y = y;} else {this.y = getRandomInt(320);}
 
 		console.log(x,y);
 		console.log(this.x,this.y);
-		this.flipped = flipped;
+		this.flipped = flipped || false;
 		// unsure if i should use a div or img here, using img bc i can
 		var element = document.createElement("img");
 		element.classList.add("img_obj");
@@ -69,6 +114,8 @@ class Object {
 		element.style.left = this.x + "px";
 		element.style.top = this.y + "px";
 		element.style["z-index"] = this.y;
+
+		dragElement(element);
 
 		// initial one for loading page.. used once... sharing when?
 		if (!this.flipped)
@@ -108,6 +155,12 @@ function changeObjectCount() {
 	var intendedCatCount 	= Number(document.getElementById("counter_cats").value) + 0;
 	var intendedCFCount 	= Number(document.getElementById("counter_campfires").value) + 0;
 	console.log(`${intendedCatCount} cats and ${intendedCFCount} campfires`);
+	if (!seen_warning){
+	if (intendedCFCount + intendedCatCount >= 80)
+	{
+		seen_warning = true;
+		window.alert("having too many cats/campfires will bring performance down, i advise you to not put many more than this, but i wont stop you. don't say i didnt warn you if the tab crashes though.\n\nthis warning won't show up twice in one session btw.");
+	}}
 
 	objects.cat.forEach(function(c)
 													{
