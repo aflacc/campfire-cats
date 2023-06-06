e8edcd1e50fc4fa73d6422632e858cf911fe00f1
aflacc <60161366+aflacc@users.noreply.github.com>
2023-06-06 21:36:30 +0000

    increase amount to cause warn, fixed border being incorrect, cats can now generate in the left and top edges, made it so the buttons get disabled when it's either working or loading. overall a general quality of life update. still no luck on figuring out how to make gifs

--- script.js
+++ script.js
@@ -3,7 +3,9 @@
 var objects = { // in different arrays so i can use the funny individual counters :3
 	cat: [],
 	campfire: []
-} 
+}
+
+var limit = 150; // at what point should it warn the user for having a lot of objects
 var preview = document.getElementById("placement_preview");
 var root = document.querySelector(':root');
 var darktheme = false;
@@ -14,51 +16,56 @@ var images = {
 	"cat": "assets/cat.png"
 }
 
+function toggleDisabled(disabled) {
+	["generateBtn", "fileType", "counter_cats", "counter_campfires","catlabel","camplabel"].forEach(function(id) {
+		document.getElementById(id)["disabled"] = disabled
+	});
+}
 // math.random is weird
 function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+	return Math.floor(Math.random() * max);
 }
 // brain hurt, thank you w3schools
 function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-  }
+	var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+	if (document.getElementById(elmnt.id + "header")) {
+		// if present, the header is where you move the DIV from:
+		document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+	} else {
+		// otherwise, move the DIV from anywhere inside the DIV:
+		elmnt.onmousedown = dragMouseDown;
+	}
 
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
+	function dragMouseDown(e) {
+		e = e || window.event;
+		e.preventDefault();
+		// get the mouse cursor position at startup:
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		document.onmouseup = closeDragElement;
+		// call a function whenever the cursor moves:
+		document.onmousemove = elementDrag;
+	}
 
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = Math.floor(elmnt.offsetTop - pos2) + "px";
+	function elementDrag(e) {
+		e = e || window.event;
+		e.preventDefault();
+		// calculate the new cursor position:
+		pos1 = pos3 - e.clientX;
+		pos2 = pos4 - e.clientY;
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		// set the element's new position:
+		elmnt.style.top = Math.floor(elmnt.offsetTop - pos2) + "px";
 		elmnt.style["z-index"] = (elmnt.offsetTop - pos2);
-    elmnt.style.left = Math.floor(elmnt.offsetLeft - pos1) + "px";
-  }
+		elmnt.style.left = Math.floor(elmnt.offsetLeft - pos1) + "px";
+	}
 
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
+	function closeDragElement() {
+		// stop moving when mouse button is released:
+		document.onmouseup = null;
+		document.onmousemove = null;
+	}
 }
 
 // unused at the moment, keeping it in case i want to use it later
@@ -98,13 +105,13 @@ function toggle_theme(actuallyChange) {
 }
 // objec t :)
 class Object {
-	constructor(type,x,y,flipped) {
+	constructor(type, x, y, flipped) {
 		this.type = type;
-		if (x != null) {this.x = x;} else {this.x = getRandomInt(600);}
-		if (y != null) {this.y = y;} else {this.y = getRandomInt(320);}
+		if (x != null) { this.x = x; } else { this.x = getRandomInt(640) - 40; }
+		if (y != null) { this.y = y; } else { this.y = getRandomInt(360) - 40; }
 
-		console.log(x,y);
-		console.log(this.x,this.y);
+		console.log(x, y);
+		console.log(this.x, this.y);
 		this.flipped = flipped || false;
 		// unsure if i should use a div or img here, using img bc i can
 		var element = document.createElement("img");
@@ -117,39 +124,33 @@ class Object {
 
 		dragElement(element);
 
-		element.ondblclick = function()
-			{
-				element.style["z-index"] = 9999;
-			}
-		
+		element.ondblclick = function() {
+			element.style["z-index"] = 9999;
+		}
+
 		// initial one for loading page.. used once... sharing when?
-		if (!this.flipped)
-				{
-					element.style["-webkit-transform"] = "scaleX(1)";
-					element.style["transform"] = "scaleX(1)";
-				}
-				else
-				{
-					element.style["-webkit-transform"] = "scaleX(-1)";
-					element.style["transform"] = "scaleX(-1)";
-				}
+		if (!this.flipped) {
+			element.style["-webkit-transform"] = "scaleX(1)";
+			element.style["transform"] = "scaleX(1)";
+		}
+		else {
+			element.style["-webkit-transform"] = "scaleX(-1)";
+			element.style["transform"] = "scaleX(-1)";
+		}
 		this.flipped = flipped;
 		// womp womp
-		element.oncontextmenu = function()
-			{
-				this.flipped = !this.flipped;
-				if (this.flipped)
-				{
-					element.style["-webkit-transform"] = "scaleX(-1)";
-					element.style["transform"] = "scaleX(-1)";
-				}
-				else
-				{
-					element.style["-webkit-transform"] = "scaleX(1)";
-					element.style["transform"] = "scaleX(1)";
-				}
-				return false; // no context menu >:(
+		element.oncontextmenu = function() {
+			this.flipped = !this.flipped;
+			if (this.flipped) {
+				element.style["-webkit-transform"] = "scaleX(-1)";
+				element.style["transform"] = "scaleX(-1)";
 			}
+			else {
+				element.style["-webkit-transform"] = "scaleX(1)";
+				element.style["transform"] = "scaleX(1)";
+			}
+			return false; // no context menu >:(
+		}
 		this.element = element;
 		document.getElementById("canvas").appendChild(element);
 		return this;
@@ -157,101 +158,87 @@ class Object {
 }
 // general handler
 function changeObjectCount() {
-	var intendedCatCount 	= Number(document.getElementById("counter_cats").value) + 0;
-	var intendedCFCount 	= Number(document.getElementById("counter_campfires").value) + 0;
+	var intendedCatCount = Number(document.getElementById("counter_cats").value) + 0;
+	var intendedCFCount = Number(document.getElementById("counter_campfires").value) + 0;
 	console.log(`${intendedCatCount} cats and ${intendedCFCount} campfires`);
-	if (!seen_warning){
-	if (intendedCFCount + intendedCatCount >= 80)
-	{
-		seen_warning = true;
-		window.alert("having too many cats/campfires will bring performance down, i advise you to not put many more than this, but i wont stop you. don't say i didnt warn you if the tab crashes though.\n\nthis warning won't show up twice in one session btw.");
-	}}
+	if (!seen_warning) {
+		if (intendedCFCount + intendedCatCount >= limit) {
+			seen_warning = true;
+			window.alert("having too many objects will cause severe performance drops (varies depending on device). you have been warned\n\ni wont warn you again in this session, i take no responsibility if your browser crashes.");
+		}
+	}
 
-	objects.cat.forEach(function(c)
-													{
-														c.element.style.display = "none";
-													})
-	objects.campfire.forEach(function(c)
-													{
-														c.element.style.display = "none";
-													})
-	for (let cat = 0; cat < intendedCatCount; cat++)
-		{
-			
-			//console.log("l")
-			if (objects["cat"][cat] == null)
-			{
-				objects["cat"].push(new Object("cat",null,null));
-			}
-			else
-			{
-				objects["cat"][cat].element.style.display = "inline-flex";
-			}
+	objects.cat.forEach(function(c) {
+		c.element.style.display = "none";
+	})
+	objects.campfire.forEach(function(c) {
+		c.element.style.display = "none";
+	})
+	for (let cat = 0; cat < intendedCatCount; cat++) {
+
+		//console.log("l")
+		if (objects["cat"][cat] == null) {
+			objects["cat"].push(new Object("cat", null, null));
 		}
-	for (let cf = 0; cf < intendedCFCount; cf++)
-		{
-			//console.log("l")
-			if (objects["campfire"][cf] == null)
-			{
-				objects["campfire"].push(new Object("campfire",null,null));
-			}
-			else
-			{
-				objects["campfire"][cf].element.style.display = "inline-flex";
-			}
+		else {
+			objects["cat"][cat].element.style.display = "inline-flex";
 		}
+	}
+	for (let cf = 0; cf < intendedCFCount; cf++) {
+		//console.log("l")
+		if (objects["campfire"][cf] == null) {
+			objects["campfire"].push(new Object("campfire", null, null));
+		}
+		else {
+			objects["campfire"][cf].element.style.display = "inline-flex";
+		}
+	}
 	console.log(objects["cat"]);
 }
-/** debug, use at own risk this will probably end reality as we know it or something */ 
-function createObject(type,x,y,flipped) {
+/** debug, use at own risk this will probably end reality as we know it or something */
+function createObject(type, x, y, flipped) {
 	console.log("you've doomed us all")
-	objects[type].push(new Object(type,x,y,flipped)); // i see no reason to assign it to variable other than to create intentional memory leaks
+	objects[type].push(new Object(type, x, y, flipped)); // i see no reason to assign it to variable other than to create intentional memory leaks
 }
 // professional programmer moments right here
-createObject("cat",110,125,false)
-createObject("campfire",210,125,false)
-createObject("cat",295,125,true)
-createObject("cat",380,125,true)
+createObject("cat", 110, 125, false)
+createObject("campfire", 210, 125, false)
+createObject("cat", 295, 125, true)
+createObject("cat", 380, 125, true)
 // and finally
 changeObjectCount()
 
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-  // true for mobile device
-  //document.write("mobile device");
-}else{
-  // false for not mobile device
-  //document.write("not mobile device");
-}
 //const canvas = document.getElementById("generated")
 //const ctx = document.getElementById("generated").getContext("2d");
 
 // wip, likely will break.
-function generateImage()
-{
+function generateImage() {
 	//document.getElementById("canvas").style.border = "none";
-	
+	toggleDisabled(true)
 	var method = document.getElementById("fileType").value;
-	switch(method)
-		{
-			case 'gif':
-				window.alert("how did you set it to gif");
-				break;
-			case 'jpeg':
-				domtoimage.toJpeg(document.getElementById('canvas'),{bgcolor: "#ffffff"})
-    .then(function (dataUrl) {
-        var link = document.createElement('a');
-        link.download = 'generated.jpeg';
-        link.href = dataUrl;
-        link.click();
-    });
-				break;
-			default: // also pngs method but theres zero reason to duplicate the code
-				domtoimage.toBlob(document.getElementById('canvas'))
-    .then(function (blob) {
-        window.saveAs(blob, 'generated.png');
-    });
-				break;
-		}
+	switch (method) {
+		case 'gif':
+			window.alert("how did you set it to gif");
+			toggleDisabled(false)
+			break;
+		case 'jpeg':
+			domtoimage.toJpeg(document.getElementById('canvas'), { bgcolor: "#ffffff" })
+				.then(function(dataUrl) {
+					var link = document.createElement('a');
+					link.download = 'generated.jpeg';
+					link.href = dataUrl;
+					link.click();
+					toggleDisabled(false)
+				});
+			break;
+		default: // also pngs method but theres zero reason to duplicate the code
+			domtoimage.toBlob(document.getElementById('canvas'))
+				.then(function(blob) {
+					window.saveAs(blob, 'generated.png');
+					toggleDisabled(false)
+				});
+			break;
+	}
 	//document.getElementById("canvas").style.border = " 2px var(--theme-border) solid";
 	console.log("Generating image!!! :3")
 	
@@ -271,5 +258,7 @@ function generateImage()
 	//window.location.href = generated.toDataURL();
 
 	console.log(generated.toDataURL());
-  window.open('', generated.toDataURL());*/
+	window.open('', generated.toDataURL());*/
 }
+
+toggleDisabled(false)
